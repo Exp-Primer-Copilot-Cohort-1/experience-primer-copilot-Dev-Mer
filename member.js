442a2aa2a5109914f0a378130973262f82f64cba
Merido Santos <40186304+Dev-Mer@users.noreply.github.com>
2023-12-14 13:36:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        "name": "Member",
+        "source": "Player's Handbook",
+        "page": 154,
+        "skill": true,
+        "modifier": "wis",
+        "modifier type": "ability",
+        "action": false,
+        "description": "Your membership in a particular adventuring guild, mercenary company, or other group grants you access to a set of benefits granted by the group. What benefits these are depends on the nature of your membership in the group. Work with your DM to determine the benefits you gain from your membership."
+    };
+}
